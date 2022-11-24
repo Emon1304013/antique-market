8ee659f82533944bfa668090470fe59c97b6d83a
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-24 08:08:34 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './router/Routes';
 // import Swal from 'sweetalert2';
 // import { useEffect } from 'react';
 
@@ -6,8 +8,8 @@ function App() {
 
   
   return (
-    <div className="App">
-      <button className="btn">Hello daisyUI</button>
+    <div className='max-w-[1440px] mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/shared/Header/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../../assets/logo.png'
+
+const Header = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1 items-center gap-2">
+        <img src={logo} alt="" className="w-12 h-12 rounded" /> 
+        <Link className="font-extrabold text-transparent text-2xl lg:text-3xl bg-clip-text bg-gradient-to-r from-green-400 to-gray-700">ANTIQUE MARKET</Link>
+      </div>
+      <div className="flex-none gap-2">
+        <Link>Login</Link>
+        <Link>Login</Link>
+        <Link>Login</Link>
+
+        {/* <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" alt="" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </Link>
+            </li>
+            <li>
+              <Link>Logout</Link>
+            </li>
+          </ul>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
