77308d3827dba9a857a068a2a7061702ccee69f1
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-26 04:51:52 +0600

    Seller Deleted successfully from front end by admin

--- src/pages/Dashboard/Admin/AllSellers.js
+++ src/pages/Dashboard/Admin/AllSellers.js
@@ -1,47 +1,80 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../../contexts/AuthProvider";
 
 const AllSellers = () => {
-    return (
-        <div>
-            <h2 className='text-2xl font-bold mb-4 text-center text-secondary uppercase'>All Sellers</h2>
+  const { user } = useContext(AuthContext);
 
-            <div className="overflow-x-auto">
-  <table className="table w-full">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
+  const {
+    data: sellers = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["sellers"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/users/sellers", {
+        headers: {
+          "content-type": "application/json",
+          authorization: `bearer ${localStorage.getItem("antique-token")}`,
+        },
+      });
+      const data = await res.json();
+      return data;
+    },
+  });
 
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
+  const handleDeleteSeller = async id => {
+    
+    const res = fetch(`http://localhost:5000/users/seller/${id}`,{
+        method:'DELETE',
+        headers:{
+            'content-type':'application/json',
+            authorization:`bearer ${localStorage.getItem('antique-token')}`
+        }
+    })
+    const data = await res.json;
+    if(data.acknowledged){
+        Swal.fire("Seller Deleted Successfully");
+        refetch();
+    }
+    else{
+        Swal.fire("Something Went Wrong")
+    }
+  }
+  console.log(sellers);
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4 text-center text-secondary uppercase">
+        All Sellers
+      </h2>
 
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-        </div>
-    );
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sellers?.map((seller,i) => (
+              <tr
+              key={seller._id}
+              >
+                <td>{i+1}</td>
+                <td>Demo Name</td>
+                <td>{seller?.email}</td>
+                <td><button onClick={()=>handleDeleteSeller(seller?._id)} className="btn btn-error">DELETE</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllSellers;
