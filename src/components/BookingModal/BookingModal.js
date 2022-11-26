005ea90104cecfdc9a5751576014e626b21d8b79
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-26 22:17:17 +0600

    Booking data stored in the database using bookings api

--- src/components/BookingModal/BookingModal.js
+++ src/components/BookingModal/BookingModal.js
@@ -1,14 +1,59 @@
-import React, { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext, useState } from "react";
 import Swal from "sweetalert2";
 import { AuthContext } from "../../contexts/AuthProvider";
 
-const BookingModal = ({product,setBooking}) => {
+const BookingModal = ({ product, setBooking }) => {
   const { user } = useContext(AuthContext);
+  const [bookedProduct,setBookedProduct] = useState('');
   console.log(user);
-  const handleSubmit = () => {
-    Swal.fire("Information Recieved. Thanks!")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userName = form.userName.value;
+    const userEmail = form.userEmail.value;
+    const productName = form.productName.value;
+    const price = form.price.value;
+    const phone = form.phone.value;
+    const location = form.location.value;
+
+    const product = {
+      userName,
+      userEmail,
+      productName,
+      price,
+      phone,
+      location,
+    }
+    setBookedProduct(product);
+    fetch(`${process.env.REACT_APP_API_URL}/bookings`,{
+      method:"POST",
+        headers:{
+          'content-type':'application/json',
+          authorization:`bearer ${localStorage.getItem('antique-token')}`,
+        },
+        body:JSON.stringify(product)
+    }).then(res => res.json()).then(data => console.log(data))
     setBooking(null);
-  }
+  };
+
+  // const {data} = useQuery({
+  //   queryKey:['bookedProduct'],
+  //   queryFn:async() => {
+  //     const res = await fetch(`${process.env.REACT_APP_API_URL}/bookings`,{
+  //       method:"POST",
+  //       headers:{
+  //         'content-type':'application/json',
+  //         authorization:`bearer ${localStorage.getItem('antique-token')}`,
+  //       },
+  //       body:JSON.stringify(bookedProduct)
+  //     })
+  //     const data = await res.json();
+  //     console.log(data);
+  //     Swal.fire("Booking Confirmed. Thanks!");
+  //     return data
+  //   }
+  // })
   return (
     <div>
       {/* The button to open modal */}
@@ -17,75 +62,86 @@ const BookingModal = ({product,setBooking}) => {
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <div>
-            <label className="label">
-              <span className="label-text">Customer Name</span>
-            </label>
-            <input
-            type="text"
-              disabled
-              defaultValue={user?.displayName}
-              className="input input-bordered w-full"
-            />
-          </div>
-          <div>
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="text"
-              disabled
-              defaultValue={user?.email}
-              className="input input-bordered w-full"
-            />
-          </div>
-          <div>
-            <label className="label">
-              <span className="label-text">Item name</span>
-            </label>
-            <input
-              type="text"
-              disabled
-              defaultValue={product.productName}
-              className="input input-bordered w-full"
-            />
-          </div>
-          <div>
-            <label className="label">
-              <span className="label-text">Price</span>
-            </label>
-            <input
-              type="text"
-              disabled
-              defaultValue={product?.price}
-              className="input input-bordered w-full"
-            />
-          </div>
-          <div>
-            <label className="label">
-              <span className="label-text">Phone Number</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Phone Number"
-              className="input input-bordered w-full"
-            />
-          </div>
-          <div>
-            <label className="label">
-              <span className="label-text">Meeting Location</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Location"
-              className="input input-bordered w-full"
-            />
-          </div>
-          <div className="modal-action">
-            <label onClick={handleSubmit} htmlFor="booking-modal" className="btn">
-              Submit
-            </label>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label className="label">
+                <span className="label-text">Customer Name</span>
+              </label>
+              <input
+                type="text"
+                name="userName"
+                disabled
+                defaultValue={user?.displayName}
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                name="userEmail"
+                disabled
+                defaultValue={user?.email}
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="label-text">Item name</span>
+              </label>
+              <input
+                type="text"
+                name="productName"
+                disabled
+                defaultValue={product.productName}
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="label-text">Price</span>
+              </label>
+              <input
+                type="text"
+                name="price"
+                disabled
+                defaultValue={product?.resellPrice}
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="label-text">Phone Number</span>
+              </label>
+              <input
+                type="text"
+                name="phone"
+                placeholder="Phone Number"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="label-text">Meeting Location</span>
+              </label>
+              <input
+                type="text"
+                name="location"
+                placeholder="Location"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div className="modal-action">
+              <button onClick={()=>setBooking(null)} className="btn">
+                Cancel
+              </button>
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
